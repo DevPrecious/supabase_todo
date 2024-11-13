488d84245b6b0dd90791af255042f93e348dc129
DevPrecious <apple@Preciouss-MacBook-Air.local>
2024-11-13 07:27:42 -0800

    [add] Update

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,8 @@ import 'package:todoapp/pages/todo_page.dart';
 
 void main() async {
   await Supabase.initialize(
-    url: 'https://fgwimcmjotakgemelupl.supabase.co',
-    anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZnd2ltY21qb3Rha2dlbWVsdXBsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE1MDgxMDYsImV4cCI6MjA0NzA4NDEwNn0.5Kb3vJfm9d7c9l9X_Yq2YN5QIfRAkXbGsU29JnU0Se0',
+    url: '',
+    anonKey: '',
   );
   runApp(const MyApp());
 }
